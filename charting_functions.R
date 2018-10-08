dcb911e865a818634566852d825bc273c6e4e931
Thomas Hirsch <thomas.hirsch@justice.gov.uk>
2018-10-08 09:55:51 +0000

    linting

--- charting_functions.R
+++ charting_functions.R
@@ -11,9 +11,9 @@ library(glue)
 get_prop_usage <- function(df_sum) {
 
   df_sum %>%
-    count(date,util_cat) %>%
+    count(date, util_cat) %>%
     group_by(date) %>%
-    mutate(prop = n/sum(n)) %>%
+    mutate(prop = n / sum(n)) %>%
     ungroup(date)
 }
 
@@ -32,17 +32,19 @@ prop_daily_usage_chart <- function(df_sum) {
   prop_usage <- get_prop_usage(df_sum)
   
   ggplot(prop_usage,
-         aes(x=date,y=prop,fill=util_cat)) +
-    geom_bar(stat="identity", position='fill') +
+         aes(x = date, y = prop, fill = util_cat)) +
+    geom_bar(stat = "identity", position = "fill") +
     ggtitle("Desk Utilisation By Date") +
-    labs(y="Desk Utilisation",fill="") +
-    labs(x=NULL,fill="") +
+    labs(y = "Desk Utilisation", fill = "") +
+    labs(x = NULL, fill = "") +
     scale_y_continuous(labels = scales::percent) +
     scale_x_date(breaks = pretty_breaks(30)) +
-    theme(legend.position="right") +
+    theme(legend.position = "right") +
     theme(plot.title = element_text(hjust = 0.5)) +
-    scale_fill_manual(values=c("Effective utilisation"="coral2","Under utilised"="thistle3","Unused"="powderblue")) +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1, size=10))
+    scale_fill_manual(values = c("Effective utilisation" = "coral2",
+                                 "Under utilised" = "thistle3",
+                                 "Unused" = "powderblue")) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 10))
   
 }
 
@@ -53,13 +55,13 @@ daily_usage_chart_narrative <- function(df_sum) {
   
   max_capacity_day_narrative <- ""
   max_capacity_days <- get_max_capacity_days(df_sum)
-  max_capacity_days_concatenated <- paste(max_capacity_days,collapse=', ')
+  max_capacity_days_concatenated <- paste(max_capacity_days, collapse = ", ")
   
-  if(length(max_capacity_days) > 0) {
+  if (length(max_capacity_days) > 0) {
     max_capacity_day_narrative <- glue("<li> Full capacity was reached on {length(max_capacity_days)} days in the sample period: {max_capacity_days_concatenated}</li>")
   }
   
-  paste0(glue("<ul><li>On average, desks were unused {percent(av_unused)} of the time during the sample period.</li>",max_capacity_day_narrative,"</ul>"))
+  paste0(glue("<ul><li>On average, desks were unused {percent(av_unused)} of the time during the sample period.</li>", max_capacity_day_narrative, "</ul>"))
   
   
 }
@@ -69,9 +71,9 @@ get_prop_usage_day <- function(df_sum) {
 
   df_sum %>%
     mutate(day = weekdays(date)) %>%
-    count(day,util_cat) %>%
+    count(day, util_cat) %>%
     group_by(day) %>%
-    mutate(prop = n/sum(n)) %>%
+    mutate(prop = n / sum(n)) %>%
     ungroup(day)
   
 }
@@ -80,13 +82,13 @@ weekday_usage_narrative <- function(df_sum) {
   prop_usage_day <- get_prop_usage_day(df_sum)
   
   daily_util <- prop_usage_day %>%
-                filter(util_cat %in% c("Effective utilisation","Under utilised")) %>%
+                filter(util_cat %in% c("Effective utilisation", "Under utilised")) %>%
                 group_by(day) %>%
                 summarise(prop = sum(prop))
   
-  top_day <- daily_util %>% filter(prop==max(prop)) %>% .$day
+  top_day <- daily_util %>% filter(prop == max(prop)) %>% .$day
   
-  friday_util <- daily_util %>% filter(day=="Friday") %>% .$prop
+  friday_util <- daily_util %>% filter(day == "Friday") %>% .$prop
   
   glue("The rate of unused desks varies over the working week with {top_day} being the busiest day on average. On average {percent(1-friday_util)} of desks are unused on Fridays.")
   
@@ -96,20 +98,22 @@ prop_weekday_usage_chart <- function(df_sum) {
   
   prop_usage_day <- get_prop_usage_day(df_sum)
   
-  weekday<-c("Monday","Tuesday","Wednesday","Thursday","Friday")
+  weekday <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
   
   ggplot(prop_usage_day,
-         aes(x=day,y=prop,fill=util_cat)) +
-    geom_bar(stat="identity", position='fill') +
+         aes(x = day, y = prop, fill = util_cat)) +
+    geom_bar(stat = "identity", position = "fill") +
     ggtitle("Desk Utilisation By Day") +
-    labs(y="Desk Utilisation",fill="") +
-    labs(x=NULL,fill="") +
+    labs(y = "Desk Utilisation", fill = "") +
+    labs(x = NULL, fill = "") +
     scale_y_continuous(labels = scales::percent) +
     scale_x_discrete(limits = weekday) +
-    theme(legend.position="right") +
+    theme(legend.position = "right") +
     theme(plot.title = element_text(hjust = 0.5)) +
-    scale_fill_manual(values=c("Effective utilisation"="coral2","Under utilised"="thistle3","Unused"="powderblue")) +
-    theme(axis.text.x = element_text(angle = 0, hjust = 0.5, size=10))
+    scale_fill_manual(values = c("Effective utilisation" = "coral2",
+                                 "Under utilised" = "thistle3",
+                                 "Unused" = "powderblue")) +
+    theme(axis.text.x = element_text(angle = 0, hjust = 0.5, size = 10))
   
   
 }
@@ -118,9 +122,9 @@ prop_weekday_usage_chart <- function(df_sum) {
 get_prop_usage_type <- function(df_sum) {
   
   df_sum %>%
-    count(devicetype,util_cat) %>%
+    count(devicetype, util_cat) %>%
     group_by(devicetype) %>%
-    mutate(prop = n/sum(n)) %>%
+    mutate(prop = n / sum(n)) %>%
     ungroup(devicetype)
 }
 
@@ -129,25 +133,27 @@ prop_desk_usage_chart <- function(df_sum) {
   prop_usage_type <- get_prop_usage_type(df_sum)
   
   ggplot(prop_usage_type,
-         aes(x=devicetype,y=prop,fill=util_cat)) +
-    geom_bar(stat="identity", position='fill') +
+         aes(x = devicetype, y = prop, fill = util_cat)) +
+    geom_bar(stat = "identity", position = "fill") +
     ggtitle("Desk Utilisation By desk type") +
-    labs(y="Desk Utilisation",fill="") +
-    labs(x=NULL,fill="") +
+    labs(y = "Desk Utilisation", fill = "") +
+    labs(x = NULL, fill = "") +
     scale_y_continuous(labels = scales::percent) +
-    theme(legend.position="right") +
+    theme(legend.position = "right") +
     theme(plot.title = element_text(hjust = 0.5)) +
-    scale_fill_manual(values=c("Effective utilisation"="coral2","Under utilised"="thistle3","Unused"="powderblue")) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, size=10))
+    scale_fill_manual(values = c("Effective utilisation" = "coral2",
+                               "Under utilised" = "thistle3",
+                               "Unused" = "powderblue")) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10))
   
 }
 
 get_prop_usage_team <- function(df_sum) {
   df_sum %>%
-    count(category_1,category_2,category_3,util_cat) %>%
-    group_by(category_1,category_2,category_3) %>%
-    mutate(prop = n/sum(n)) %>%
-    ungroup(category_1,category_2,category_3)
+    count(category_1, category_2, category_3, util_cat) %>%
+    group_by(category_1, category_2, category_3) %>%
+    mutate(prop = n / sum(n)) %>%
+    ungroup(category_1, category_2, category_3)
   
 }
 
@@ -156,25 +162,27 @@ prop_team_usage_chart <- function(df_sum) {
   prop_usage_team <- get_prop_usage_team(df_sum)
   
   ggplot(prop_usage_team,
-         aes(x=category_3,y=prop,fill=util_cat)) +
-    geom_bar(stat="identity", position='fill') +
+         aes(x = category_3, y = prop, fill = util_cat)) +
+    geom_bar(stat = "identity", position = "fill") +
     ggtitle("Desk Utilisation By Team") +
-    labs(y="Desk Utilisation",fill="") +
-    labs(x=NULL,fill="") +
+    labs(y = "Desk Utilisation", fill = "") +
+    labs(x = NULL, fill = "") +
     scale_y_continuous(labels = scales::percent) +
-    theme(legend.position="right") +
+    theme(legend.position = "right") +
     theme(plot.title = element_text(hjust = 0.5)) +
-    scale_fill_manual(values=c("Effective utilisation"="coral2","Under utilised"="thistle3","Unused"="powderblue")) +
-    theme(axis.text.x = element_text(angle = 0, hjust = 0.5, size=10))
+    scale_fill_manual(values = c("Effective utilisation" = "coral2",
+                                 "Under utilised" = "thistle3",
+                                 "Unused" = "powderblue")) +
+    theme(axis.text.x = element_text(angle = 0, hjust = 0.5, size = 10))
 }
 
 
 get_prop_usage_floor <- function(df_sum) {
   
   df_sum %>%
-    count(floor,util_cat) %>%
+    count(floor, util_cat) %>%
     group_by(floor) %>%
-    mutate(prop = n/sum(n)) %>%
+    mutate(prop = n / sum(n)) %>%
     ungroup(floor)
 }
 
@@ -183,38 +191,43 @@ prop_floor_usage_chart <- function(df_sum) {
   prop_usage_floor <- get_prop_usage_floor(df_sum)
   
   ggplot(prop_usage_floor,
-         aes(x=factor(floor),y=prop,fill=util_cat)) +
-    geom_bar(stat="identity", position='fill') +
+         aes(x = factor(floor), y = prop, fill = util_cat)) +
+    geom_bar(stat = "identity", position = "fill") +
     ggtitle("Desk Utilisation By Floor") +
-    labs(y="Desk Utilisation",fill="") +
-    labs(x=NULL,fill="") +
+    labs(y = "Desk Utilisation", fill = "") +
+    labs(x = NULL, fill = "") +
     scale_y_continuous(labels = scales::percent) +
-    theme(legend.position="right") +
+    theme(legend.position = "right") +
     theme(plot.title = element_text(hjust = 0.5)) +
-    scale_fill_manual(values=c("Effective utilisation"="coral2","Under utilised"="thistle3","Unused"="powderblue")) +
-    theme(axis.text.x = element_text(angle = 0, hjust = 0.5, size=10))
+    scale_fill_manual(values = c("Effective utilisation" = "coral2",
+                                 "Under utilised" = "thistle3",
+                                 "Unused" = "powderblue")) +
+    theme(axis.text.x = element_text(angle = 0, hjust = 0.5, size = 10))
   
   
 }
 
-get_smoothing_table <- function(df_sum,smoothing_factor) {
+get_smoothing_table <- function(df_sum, smoothing_factor) {
   
-  weekdays <-c("Monday","Tuesday","Wednesday","Thursday","Friday")
+  weekdays <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
   
-  cat_order <- c("Full Smoothing","Current Utilisation","Partial Smoothing")
+  cat_order <- c("Full Smoothing", "Current Utilisation", "Partial Smoothing")
   
   smoothing <- get_prop_usage_day(df_sum) %>%
     filter(util_cat == "Unused") %>%
-    mutate(current_utilisation = 1- prop) %>%
+    mutate(current_utilisation = 1 - prop) %>%
     mutate(full_smoothing = mean(current_utilisation)) %>%
-    mutate(partial_smoothing = (full_smoothing * smoothing_factor) + (current_utilisation * (1-smoothing_factor))) %>%
-    select(day,"Full Smoothing" = full_smoothing,"Current Utilisation" = current_utilisation, "Partial Smoothing" = partial_smoothing) %>%
+    mutate(partial_smoothing = (full_smoothing * smoothing_factor) + (current_utilisation * (1 - smoothing_factor))) %>%
+    select(day,
+           "Full Smoothing" = full_smoothing, 
+           "Current Utilisation" = current_utilisation,
+           "Partial Smoothing" = partial_smoothing) %>%
     melt("day") %>%
-    mutate(variable = factor(variable,cat_order))
+    mutate(variable = factor(variable, cat_order))
 }
 
-get_smoothing_narrative <- function(df_sum,smoothing_factor) {
-  smoothing_table <- get_smoothing_table(df_sum,smoothing_factor)
+get_smoothing_narrative <- function(df_sum, smoothing_factor) {
+  smoothing_table <- get_smoothing_table(df_sum, smoothing_factor)
   
   smoothing_table_wide <- smoothing_table %>% 
     dcast(day ~ variable) %>%
@@ -230,23 +243,25 @@ get_smoothing_narrative <- function(df_sum,smoothing_factor) {
 smoothing_chart <- function(df_sum, smoothing_factor) {
   
   
-  weekdays <-c("Monday","Tuesday","Wednesday","Thursday","Friday")
+  weekdays <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
   
-  cat_order <- c("Full Smoothing","Current Utilisation","Partial Smoothing")
+  cat_order <- c("Full Smoothing", "Current Utilisation", "Partial Smoothing")
 
   smoothing <- get_smoothing_table(df_sum,smoothing_factor)
 
   
   ggplot(smoothing,
-         aes(x=day,y=value,fill=variable)) + 
-    geom_bar(stat="identity",position="dodge") + 
+         aes(x = day, y = value, fill = variable)) + 
+    geom_bar(stat = "identity", position = "dodge") + 
     ggtitle("Required Desk Allocation - Smoothing Assumptions") + 
     scale_x_discrete(limits = weekdays) + 
-    theme(legend.position="top") + 
+    theme(legend.position = "top") + 
     theme(plot.title = element_text(hjust = 0.5)) + 
-    expand_limits(y=0)+scale_y_continuous(expand = c(0, 0),labels = percent) +
-    coord_cartesian(ylim=c(0,1))+labs(y="Desk Utilisation",fill="") + 
-    scale_fill_brewer(palette="Accent")
+    expand_limits(y = 0) + 
+    scale_y_continuous(expand = c(0, 0),labels = percent) +
+    coord_cartesian(ylim = c(0, 1)) + 
+    labs(y="Desk Utilisation",fill="") + 
+    scale_fill_brewer(palette = "Accent")
   
 }
 
@@ -255,17 +270,17 @@ allocation_strategy_table <- function(df_sum) {
   
   current_allocation <- n_distinct(df_sum$surveydeviceid)
   
-  prop_usage <- prop.table(table(df_sum$date,df_sum$util_cat),1)
-  prop_usage_day <- prop.table(table(weekdays(df_sum$date),df_sum$util_cat),1)
+  prop_usage <- prop.table(table(df_sum$date, df_sum$util_cat), 1)
+  prop_usage_day <- prop.table(table(weekdays(df_sum$date), df_sum$util_cat), 1)
   
   min_unused <- min(prop_usage[,"Unused"])
   
   av.unutilise <- mean(prop_usage_day[,"Unused"])
-  full_smoothing_factor <- 1-av.unutilise
+  full_smoothing_factor <- 1 - av.unutilise
   
   daily_utilisation <- 1 - prop_usage_day[,"Unused"]
   
-  smoothing <- (daily_utilisation + full_smoothing_factor)/2
+  smoothing <- (daily_utilisation + full_smoothing_factor) / 2
   
   partial_smoothing_factor <- max(smoothing)
   
@@ -277,7 +292,7 @@ allocation_strategy_table <- function(df_sum) {
   dark_blue <- current_allocation * (1-min_unused)
   green <- current_allocation * partial_smoothing_factor
   second_green <- current_allocation * full_smoothing_factor
-  yellow <- second_green - ((1 - hotdesk_ratio)*current_allocation*mean_underutilised)
+  yellow <- second_green - ((1 - hotdesk_ratio) * current_allocation * mean_underutilised)
   orange <- daily_utilisation["Friday"] * current_allocation
   
   recommendation_list <- c("Current Allocation",
@@ -286,14 +301,19 @@ allocation_strategy_table <- function(df_sum) {
                            glue("If you were to fully smooth working patterns over the week, you could save a further {round(green - second_green)} desks"),
                            glue("On average {round(mean_underutilised * 100)}% of desks were used inefficiently. By embedding a culture of hotdesking (assuming a desk-to-person ratio of 0.6) it would be possible to replace {round(current_allocation * mean_underutilised)} desks with {round(current_allocation * mean_underutilised * hotdesk_ratio)} desks."),
                            "The average amount of desks effectively utilised and under utilised on Friday in the survey period.")
-  desks_in_scope <- c(current_allocation,round(dark_blue),round(green),round(second_green),round(yellow),round(orange))
+  desks_in_scope <- c(current_allocation,
+                      round(dark_blue),
+                      round(green),
+                      round(second_green),
+                      round(yellow),
+                      round(orange))
   scenario_saving <- c(0,
                        round(current_allocation - dark_blue),
                        round(current_allocation - green),
                        round(current_allocation - second_green),
                        round(current_allocation - yellow),
                        round(current_allocation - orange))
-  percent_current_allocation <- paste(round((desks_in_scope/current_allocation)*100),"%",sep="")
+  percent_current_allocation <- paste(round((desks_in_scope / current_allocation)*100), "%", sep="")
   
   out <- data.frame("recommendation" = recommendation_list,
                     "desks in scope" = desks_in_scope,
@@ -321,9 +341,9 @@ desks_by_team <- function(df_sum) {
 
 desks_by_desk_type_and_team <- function(df_sum) {
   df_sum %>%
-    group_by(category_3,devicetype) %>%
+    group_by(category_3, devicetype) %>%
     summarise(sensors = n_distinct(surveydeviceid)) %>%
-    rename("Desk Type" = devicetype,"team"=category_3)
+    rename("Desk Type" = devicetype, "team" = category_3)
 }
 
 get_peak_occupancy <- function(df_sum) {
@@ -332,10 +352,10 @@ get_peak_occupancy <- function(df_sum) {
   prop_usage %>% 
     filter(util_cat != "Unused") %>%
     group_by(date) %>%
-    summarise(prop=sum(prop)) %>%
-    mutate(date = as.character(date),utilisation=percent(prop)) %>% 
+    summarise(prop = sum(prop)) %>%
+    mutate(date = as.character(date), utilisation = percent(prop)) %>% 
     arrange(desc(prop)) %>%
-    select(date,utilisation) %>%
+    select(date, utilisation) %>%
     head(10)
 }
 
