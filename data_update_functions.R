7e9507713bdae3d3213a70305d7a52ae0b400976
ahasham-smith <azhar.hasham-smith@justice.gov.uk>
2018-11-15 16:24:29 +0000

    add function to update selected surveys

---
+++ data_update_functions.R
@@ -0,0 +1,19 @@
+library(feather)
+
+
+# replace this with the updated list of surveys
+survey_list <- c("102 Petty France v2.1",
+                 "10 South Colonnade",
+                 "5WP; Leeds",
+                 "HMPPS in Southern House & 102PF",
+                 "Southern House - Home Office",
+                 "Apollo House - Home Office")
+
+update_selected_survey_list <- function(survey_list) {
+
+my_df <- data.frame(surveyname = survey_list)
+feather::write_feather(my_df, "selected surveys.feather")
+s3tools::write_file_to_s3("selected surveys.feather", "alpha-app-occupeye-automation/selected surveys.feather", overwrite = TRUE)
+}
+
+update_selected_survey_list(survey_list)
