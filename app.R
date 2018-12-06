93c8d4342d5097aaf7c6da4275e9f32a6414e950
Thomas Hirsch <thomas.hirsch@justice.gov.uk>
2018-12-06 10:01:30 +0000

    limit survey list to selected surveys

--- app.R
+++ app.R
@@ -33,13 +33,18 @@ device_types <- unique(temp_df$devicetype)
 floors <- unique(temp_df$floor)
 
 
-# Get the surveys table, and make a dictionary of survey names to their IDs. 
-# So calling surveys_hash["survey_name"] returns its corresponding survey_id
-surveys_list <- s3tools::read_using(FUN = feather::read_feather, s3_path = "alpha-app-occupeye-automation/surveys.feather")
-surveys_hash <- with(surveys_list[c("name", "survey_id")], setNames(survey_id, name))
-
 # Get the list of active survey
 active_surveys <- s3tools::read_using(FUN = feather::read_feather, s3_path = "alpha-app-occupeye-automation/active surveys.feather")
+
+# Get the surveys table, and make a dictionary of survey names to their IDs. 
+# So calling surveys_hash["survey_name"] returns its corresponding survey_id
+surveys_list <- s3tools::read_using(FUN = feather::read_feather, s3_path = "alpha-app-occupeye-automation/surveys.feather") %>%
+  filter(name %in% active_surveys$surveyname)
+surveys_hash <- with(surveys_list[c("name", "survey_id")], setNames(survey_id, name))
+
+
+
+
 selected_survey_id <- surveys_hash[1]
 
 # Get the list of reports in the folder of the selected file
