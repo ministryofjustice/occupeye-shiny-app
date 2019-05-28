629958597975c0d859ec6b7b5c56f2cb99e777a5
Thomas-Hirsch <37899506+Thomas-Hirsch@users.noreply.github.com>
2019-05-28 14:03:12 +0100

    Update data_update_functions.R

--- data_update_functions.R
+++ data_update_functions.R
@@ -2,7 +2,11 @@ library(feather)
 
 
 # replace this with the updated list of surveys
-survey_list <- c("102 Petty France v2.3b",
+survey_list <- c("LAA Nottingham",
+                 "OPG Nottingham",
+                 "102 Petty France April 19",
+                 "102 Petty France March 19",
+                 "102 Petty France v2.3b",
                  "10 South Colonnade",
                  "5WP; Leeds",
                  "HMPPS in Southern House v2.0",
@@ -13,10 +17,10 @@ survey_list <- c("102 Petty France v2.3b",
                  "Edward House; APVU")
 
 update_selected_survey_list <- function(survey_list) {
-
-my_df <- data.frame(surveyname = survey_list)
-feather::write_feather(my_df, "active surveys.feather")
-s3tools::write_file_to_s3("active surveys.feather", "alpha-app-occupeye-automation/active surveys.feather", overwrite = TRUE)
+  
+  my_df <- data.frame(surveyname = survey_list)
+  feather::write_feather(my_df, "active surveys.feather")
+  s3tools::write_file_to_s3("active surveys.feather", "alpha-app-occupeye-automation/active surveys.feather", overwrite = TRUE)
 }
 
 update_selected_survey_list(survey_list)
